b47ba5a1c0fcf93fec1282183425161a3029ed38
John Rusnak <jrusnak@linkedin.com>
2018-01-03 09:13:08 -0800

    rename to apk_bitminer

--- apk_bitminer/setup.py
+++ apk_bitminer/setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     package_dir={'': 'src'},
     packages=setuptools.find_packages('src'),
     include_package_data=True,
-    namespace_packages=['androidtools'],
-    scripts=['src/androidtools/apkdump/pydexdump',
-             'src/androidtools/apkdump/axmldump']
+    namespace_packages=[],
+    scripts=['src/apk_bitminer/pydexdump',
+             'src/apk_bitminer/axmldump']
 )
